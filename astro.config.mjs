090e2bad8492bdb063aa848fc2ed4d9447eb4978
Nassim Rahali <nassim@mentis-consulting.be>
2024-06-24 21:32:47 +0200

    fix: base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nashimu.github.io",
+  site: "https://nashimu89.github.io",
   integrations: [mdx(), sitemap()],
-  base: "nashimu",
 });
